c9454444d29147a9bd41829333d0852b6ac5d31e
Jaewon <soul0914@naver.com>
2017-03-29 15:06:26 +0900

    2장 추가

---
+++ PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using PartyInvites.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Gooood Morning" : "Good Afternoon";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if(ModelState.IsValid) { 
+                // TODO : 파티 주최자에게 전자 메일로 응답한다.
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                // 유효성 검사 오류가 존재한다.
+                return View();
+            }
+        }
+    }
+}
