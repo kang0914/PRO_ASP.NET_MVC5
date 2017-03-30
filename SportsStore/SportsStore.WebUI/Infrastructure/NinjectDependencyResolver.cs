49ed63bcbbe793733db00679870ecf117b3dc8c1
Jaewon <soul0914@naver.com>
2017-03-30 15:09:19 +0900

    7장. SportsStore : 실무 응용프로그램 - 도메인 모델 작성하기

--- SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using Ninject;
 using System.Web.Mvc;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
 
 namespace SportsStore.WebUI.Infrastructure
 {
@@ -29,7 +32,14 @@ namespace SportsStore.WebUI.Infrastructure
 
         private void AddBinding()
         {
-            // 여기에 바인딩 정보를 입력한다.
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new List<Product> {
+                new Product {Name = "Football", Price = 25 },
+                new Product {Name = "Surf board", Price = 179 },
+                new Product {Name = "Running shoes", Price = 95 },
+            });
+
+            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
         }
     }
 }
