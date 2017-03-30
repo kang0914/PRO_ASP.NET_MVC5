bf5b6488d192bfb9663b2c39a02122076b8e5d74
Jaewon <soul0914@naver.com>
2017-03-30 19:03:15 +0900

    7장. SportsStore : 실무 응용프로그램 - 데이터베이스 준비하기

--- SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -7,6 +7,7 @@ using System.Web.Mvc;
 using Moq;
 using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
+using SportsStore.Domain.Concrete;
 
 namespace SportsStore.WebUI.Infrastructure
 {
@@ -32,14 +33,7 @@ namespace SportsStore.WebUI.Infrastructure
 
         private void AddBinding()
         {
-            Mock<IProductRepository> mock = new Mock<IProductRepository>();
-            mock.Setup(m => m.Products).Returns(new List<Product> {
-                new Product {Name = "Football", Price = 25 },
-                new Product {Name = "Surf board", Price = 179 },
-                new Product {Name = "Running shoes", Price = 95 },
-            });
-
-            kernel.Bind<IProductRepository>().ToConstant(mock.Object);
+            kernel.Bind<IProductRepository>().To<EFProductRepository>();
         }
     }
 }
