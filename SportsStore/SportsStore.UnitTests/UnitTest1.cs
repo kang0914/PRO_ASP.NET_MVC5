0d5a4d45c110d371d2c172d6514d5025784ba238
Jaewon <soul0914@naver.com>
2017-03-31 10:35:56 +0900

    7장. SportsStore : 실무 응용프로그램 - 페이징 추가하기

--- SportsStore/SportsStore.UnitTests/UnitTest1.cs
+++ SportsStore/SportsStore.UnitTests/UnitTest1.cs
@@ -1,5 +1,15 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Controllers;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using SportsStore.WebUI.Models;
+using SportsStore.WebUI.HtmlHelpers;
 
 namespace SportsStore.UnitTests
 {
@@ -7,8 +17,84 @@ namespace SportsStore.UnitTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Can_Paginate()
         {
+            // Arrange
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product { ProductID = 1, Name = "P1"},
+                new Product { ProductID = 2, Name = "P2"},
+                new Product { ProductID = 3, Name = "P3"},
+                new Product { ProductID = 4, Name = "P4"},
+                new Product { ProductID = 5, Name = "P5"},
+            });
+
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            // Act
+            ProductsListViewModel result = (ProductsListViewModel)controller.List(2).Model;
+
+            // Assert
+            Product[] prodArray = result.Products.ToArray();
+            Assert.IsTrue(prodArray.Length == 2);
+            Assert.AreEqual(prodArray[0].Name, "P4");
+            Assert.AreEqual(prodArray[1].Name, "P5");
+        }
+
+        [TestMethod]
+        public void Can_Generate_Page_Links()
+        {
+            // Arrange - 확장 메서드를 적용하기 위한 HTML 헬퍼를 정의한다.
+            HtmlHelper myHelper = null;
+
+            // Arrange - PagingInfo 데이터를 생성한다.
+            PagingInfo pagingInfo = new PagingInfo
+            {
+                CurrentPage = 2,
+                TotalItems = 28,
+                ItemsPerPage = 10
+            };
+
+            // Arrange - 람다 표현식을 사용해서 델리게이트를 설정한다.
+            Func<int, string> pageurlDelegate = i => "Page" + i;
+
+            // Act
+            MvcHtmlString result = myHelper.PageLinks(pagingInfo, pageurlDelegate);
+
+            // Assert
+            Assert.AreEqual(@"<a class=""btn btn-default"" href=""Page1"">1</a>" +
+                            @"<a class=""btn btn-default btn-primary selected"" href=""Page2"">2</a>" +
+                            @"<a class=""btn btn-default"" href=""Page3"">3</a>", result.ToString());            
+        }
+
+        [TestMethod]
+        public void Can_Send_Pagination_View_Model()
+        {
+            // Arrange
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] {
+                new Product { ProductID = 1, Name = "P1" },
+                new Product { ProductID = 2, Name = "P2" },
+                new Product { ProductID = 3, Name = "P3" },
+                new Product { ProductID = 4, Name = "P4" },
+                new Product { ProductID = 5, Name = "P5" },
+            });
+
+            // Arrange
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            // Act
+            ProductsListViewModel result = (ProductsListViewModel)controller.List(2).Model;
+
+            // Assert
+            PagingInfo pageInfo = result.PagingInfo;
+            Assert.AreEqual(pageInfo.CurrentPage, 2);
+            Assert.AreEqual(pageInfo.ItemsPerPage, 3);
+            Assert.AreEqual(pageInfo.TotalItems, 5);
+            Assert.AreEqual(pageInfo.TotalPages, 2);
         }
     }
 }
