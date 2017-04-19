f05989cb5bab31232c114da3f03a9abcd2306794
Jaewon <soul0914@naver.com>
2017-04-19 14:53:40 +0900

    10장. SportsStore: 모바일 추가

--- SportsStore/SportsStore.WebUI/Controllers/NavController.cs
+++ SportsStore/SportsStore.WebUI/Controllers/NavController.cs
@@ -24,8 +24,8 @@ namespace SportsStore.WebUI.Controllers
                                                         .Select(x => x.Category)
                                                         .Distinct()
                                                         .OrderBy(x => x);
-
-            return PartialView(categories);
+            
+            return PartialView("FlexMenu", categories);
         }
     }
 }
